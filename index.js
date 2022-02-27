efb229fcd062303f162cc5e64c5b8772126713ff
Emma Perlman <emmajperlman@gmail.com>
2022-02-26 17:41:46 -0800

    commit tree 45aa746326ca96f21efa9df0f72235b9356946e7

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(userName,userEmail){
+    const userObj={
+        name:userName,
+        email:userEmail
+    }
+    function appendDOMID(obj){
+        let id= document.createElement('p')
+        document.querySelector('body').appendChild(id)
+        id.textContent=obj.id
+    }
+    function appendDOMError(error){
+        let errorMessage= document.createElement('p')
+        document.querySelector('body').appendChild(errorMessage)
+        errorMessage.textContent=error.message
+    }
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            Accept:'application/json',
+        },
+        body: JSON.stringify(userObj),
+    })
+    .then(res=>res.json())
+    .then(obj=>appendDOMID(obj))
+    .catch(error=>appendDOMError(error))
+ 
+}
+ 
